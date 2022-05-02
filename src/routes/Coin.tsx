41b9769e6ee71d49416a207d917e5eedf770c3ec
task11 <task11@naver.com>
2022-05-03 00:14:24 +0900

    feat: Render coin info data with styled-components

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -25,6 +25,31 @@ const Loader = styled.div`
   display: block;
 `;
 
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  span:first-child{
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
 interface IRouteState {
   state: {
     name: string;
@@ -111,12 +136,41 @@ function Coin() {
   return (
     <Container>
       <Header>
-        <Title>{state?.name || "Loding.."}</Title>
+        <Title>{state?.name ? state.name : loading ? "Loding.." : info?.name}</Title>
       </Header>
       {
         loading ?
-          <Loader>Loading!!!!</Loader>
-          : <span>{ }</span>
+          <Loader>Loading...</Loader>
+          :
+          <>
+            <Overview>
+              <OverviewItem>
+                <span>Rank:</span>
+                <span>{info?.rank}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>Symbol:</span>
+                <span>{info?.symbol}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>Open Source:</span>
+                <span>{info?.open_source ? "YES" : "NO"} </span>
+              </OverviewItem>
+            </Overview>
+            <Description>
+              {info?.description}
+            </Description>
+            <Overview>
+              <OverviewItem>
+                <span>Total Suply:</span>
+                <span>{priceInfo?.total_supply}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>Max Supply:</span>
+                <span>{priceInfo?.max_supply}</span>
+              </OverviewItem>
+            </Overview>
+          </>
       }
     </Container>
 
