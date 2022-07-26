a772c743fca5d97defce0a22c40cb11ada491383
task11 <task11@naver.com>
2022-07-26 18:57:23 +0900

    refactor: react-query로 코드 리팩토링

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import { theme } from './theme';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 
 );
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { fetchCoins } from "../api";
 
 
 const Container = styled.div`
@@ -56,9 +58,6 @@ const Img = styled.img`
   margin-right: 10px;
 `;
 
-
-
-
 interface CoinInterface {
   id: string,
   name: string,
@@ -70,27 +69,18 @@ interface CoinInterface {
 }
 
 function Coins() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const { isLoading, data } = useQuery<CoinInterface[]>("allCoins", fetchCoins);
 
-  useEffect(() => {
-    (async () => {
-      const response = await fetch("https://api.coinpaprika.com/v1/coins");
-      const json = await response.json();
-      setCoins(json.slice(0, 100));
-      setLoading(false);
-    })();
-  }, []);
   return (
     <Container>
       <Header>
         <Title>Coin Tracker</Title>
       </Header>
       {
-        loading ?
+        isLoading ?
           <Loader>Loading!!!!</Loader>
           : <CoinsList>
-            {coins.map(coin => (
+            {data?.slice(0, 100).map(coin => (
               <Coin key={coin.id}>
                 <Link
                   to={`/${coin.id}`}
